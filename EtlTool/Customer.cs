02e80af792e62257ab4e3ed2f2b0416ab095bf0c
Alex Malik <oleksii.malyk@gmail.com>
2021-07-13 11:55:49 +0300

    Add comments and suggestions

--- EtlTool/Customer.cs
+++ EtlTool/Customer.cs
@@ -2,6 +2,39 @@
 {
     public class Customer
     {
-        public string Id, FirstName, LastName, PhoneNumber;
+        // It's a bad practice to use public fields. Use public properties instead.
+        // Reason: fields are visible to everyone and you're not in control of when
+        // and how they're modified. But with properties you can control access. 
+        
+        // If some properties of the object are not intended to be changed after
+        // initialization, but might be used as readonly value, then you can use
+        // get-only property.
+        
+        // I changed a bit your class to show you an example:
+
+        // Now the unique identifier of a customer has public getter and setter and 
+        // can be safely accessed outside of the class.
+        public string Id { get; set; }
+
+        // And the first name of the customer now has the public getter and setter
+        // so it can be modified outside of the class.
+        public string FirstName { get; set; }
+        
+        // TODO I let you to decide what to do with these two field.
+        public string LastName, PhoneNumber;
+        
+        
+        // Another example of how the properties could be used:
+        /*
+            // Public constructor initialized with a unique identifier of a customer. 
+            public Customer(string id)
+            {
+                // The Id property is readonly and can be initialized only from constructor.
+                Id = id;
+            }
+            
+            // The readonly property that can only be read be user of the class.
+            public string Id { get; }
+        */
     }
 }
--- EtlTool/CustomerCsv.cs
+++ EtlTool/CustomerCsv.cs
@@ -33,6 +33,7 @@ namespace EtlTool
             int index = 0;
             foreach(var row in rows)
             {
+                // What if value in some cell will contain the same symbol as delimiter? 
                 var columns = row.Split(_delimeter);
                 //if index is zero define the columns
                 if (index == 0)
@@ -44,6 +45,23 @@ namespace EtlTool
                 }
                 else
                 {
+                    // I believe your program will become more adoptable to changes if you split 
+                    // the part that parses the file and the part that maps it to some entity. 
+                    // In current implementation you duplicate the process of parsing in CustomerCsv and TaskCsv.
+                    // Consider create CsvParser or CsvReader and put all logic related to data reading inside it.
+                    // And map data to particular model somewhere else.
+                    
+                    // This is change is also a part of SOLID principals.
+                    // The Single Responsibility Principal.
+                    
+                    // Right now this method does a lot of things:
+                    // - it reads file
+                    // - then decodes it
+                    // - then parses decoded data
+                    // - and then maps it to an entity
+                    // So every change of any of these parts will trigger a change of this method. Try to split logic
+                    // to different classes and method by their responsibilities.
+                    
                     var customer = new Customer();
                     for (var i = 0; i < columns.Length; i++)
                     {
